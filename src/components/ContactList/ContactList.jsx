9fa7530de2fd767de98d2b3dab2e477779b8e4a9
Yurii Denkovych <yd2004@ukr.net>
2022-08-10 16:04:36 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -5,20 +5,7 @@ import style from './ContactList.module.css';
 export default function ContactList({ contacts, filter, deleteContact }) {
   return (
     <ul className={style.contactList}>
-      {filter
-        ? contacts
-            .filter(({ name }) =>
-              name.toLowerCase().includes(filter.toLowerCase())
-            )
-            .map(({ name, number, id }) => (
-              <ContactListItem
-                name={name}
-                number={number}
-                key={id}
-                handleClick={() => deleteContact(id)}
-              />
-            ))
-        : contacts.map(({ name, number, id }) => (
+      {contacts.map(({ name, number, id }) => (
             <ContactListItem
               name={name}
               number={number}
